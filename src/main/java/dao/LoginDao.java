2f861af92f21941afc98b53ea89d8df00a69b5cb
Breno Oliveira <1brenomiranda@gmail.com>
2025-06-29 15:22:21 -0300

    classe LoginDao

--- src/main/java/dao/LoginDao.java
+++ src/main/java/dao/LoginDao.java
@@ -4,12 +4,37 @@
  */
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Breno
  */
 public class LoginDao {
     
-  //  Conexao conexao = new Conexao();
+    private Conexao conexao;
+    public LoginDao() throws SQLException {
+        conexao = new Conexao(); 
+    }
+    
+    
+    
+     /*public Login buscarUsuario(String usuario, String senha) throws SQLException {
+        String sql = "select * from login where usuario = ? and senha = ?";
+        PreparedStatement preparedStatement = conexao.prepareStatement(sql);
+        preparedStatement.setString(1, usuario);
+        preparedStatement.setString(2, senha);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next() == true) {
+            Login login = new Login();
+            login.setUsuario(resultSet.getString("usuario"));
+            login.setSenha(resultSet.getString("senha"));
+            return login;
+        }
+        return null;
+    }*/
     
 }
